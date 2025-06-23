a858d8e9d2a53b519c778696148b0c13776df989
Ruslan379 <103874267+Ruslan379@users.noreply.github.com>
2025-06-23 08:44:01 +0000

    Deploying to gh-pages from @ Ruslan379/vite-course-fe4-libraries@9868ea33c528b5a6b47d38b6a49536e3333f63a8 🚀

--- assets/lesson-07-CSoK5o69.js
+++
@@ -1,3 +0,0 @@
-import"./reset-CdHmiPVj.js";//! Урок-07 - приклад використання бібліотеки Handlebars в проєкті Vite
-console.log("%c LESSON-07 - Використання бібліотеки Handlebars ","color: white; background-color: #D33F49");//! Handlebars
-console.warn("Бібліотека Handlebars:");
---
+++ assets/lesson-07-DNyjULJg.js
@@ -0,0 +1,3 @@
+import"./reset-CdHmiPVj.js";//! Урок-07 - приклад використання бібліотеки Handlebars в проєкті Vite
+console.log("Підключено скрипт lesson-07.js");console.log("%c LESSON-07 - Використання бібліотеки Handlebars ","color: white; background-color: #D33F49");//! Handlebars
+console.warn("Бібліотека Handlebars:");

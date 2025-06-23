1f41c0fc0b4ecd73ab44d58b289c24931ff867df
Ruslan <arnov@i.ua>
2025-06-23 10:36:02 +0300

    Step-7

---
+++ src/js/lesson-07.js
@@ -0,0 +1,12 @@
+//! Урок-07 - приклад використання бібліотеки Handlebars в проєкті Vite
+import '../css/reset.css'
+import '../css/lesson-07.css'
+
+console.log(
+    "%c LESSON-07 - Використання бібліотеки Handlebars ",
+    "color: white; background-color: #D33F49",
+);
+
+//! Handlebars
+console.warn("Бібліотека Handlebars:");
+

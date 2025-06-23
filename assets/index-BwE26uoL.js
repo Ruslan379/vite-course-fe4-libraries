65f609c65dc49c3a6d6c90ec4dcfdcb3a7e2f6e1
Ruslan379 <103874267+Ruslan379@users.noreply.github.com>
2025-06-23 11:40:32 +0000

    Deploying to gh-pages from @ Ruslan379/vite-course-fe4-libraries@5a1e6551ac626389b5d078d7349e920d86132400 🚀

--- assets/index-BwE26uoL.js
+++ assets/index-BwE26uoL.js
@@ -1,2 +1,2 @@
-import"./reset-CdHmiPVj.js";//! Головний скрипт
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css              *///! Головний скрипт
 console.log("Підключено скрипт main.js");//! Імпорти додаткових скриптів в проєкт
--- assets/lesson-07-BveM2a7c.js
+++
@@ -1,3 +0,0 @@
-import"./reset-CdHmiPVj.js";//! Урок-07 - приклад використання бібліотеки Handlebars в проєкті Vite
-console.log("Підключено скрипт lesson-07.js");console.log("%c LESSON-07 - Використання бібліотеки Handlebars ","color: white; background-color: #D33F49");//! Handlebars
-console.warn("Бібліотека Handlebars:");const o={title:"Eat it createElement, templates rule!",items:["Handlebars","LoDash","Pug","EJS","lit-html"]},n=document.querySelector("#menu-template").innerHTML.trim(),t=Handlebars.compile(n),e=t(o);console.log(e);const l=document.querySelector("#menu-container");l.innerHTML=e;

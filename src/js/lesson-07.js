e0397c9ca00d255f55cb0eccad153cb5117e8ca1
Ruslan <arnov@i.ua>
2025-06-23 12:33:52 +0300

    Step-9

--- src/js/lesson-07.js
+++ src/js/lesson-07.js
@@ -11,4 +11,33 @@ console.log(
 
 //! Handlebars
 console.warn("Бібліотека Handlebars:");
+const menuData = {
+    title: 'Eat it createElement, templates rule!',
+    items: ['Handlebars', 'LoDash', 'Pug', 'EJS', 'lit-html']
+};
+
+const source = document.querySelector('#menu-template').innerHTML.trim();
+const template = Handlebars.compile(source);
+
+/*
+  Создадим переменную markup, в которую запишем 
+  результат работы функции-шаблона template
+*/
+const markup = template(menuData);
+
+console.log(markup); // магия
+/*
+  Пример выше похож на операцию «поиск-и-замена»: шаблон просто 
+  заменил {{title}} на значение свойства menuData.title. 
+  После чего в результат попал сначала текст <ul>, 
+  потом выполнился {{#each items}}, который последовательно 
+  сгенерировал элементы списка, и затем список был закрыт </ul>.
+*/
+
+/*
+  В js выберем контейнер по id, после чего используя 
+  свойство innerHTML присвоим туда нашу строку.
+*/
+const container = document.querySelector('#menu-container');
+container.innerHTML = markup;
 

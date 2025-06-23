4cd698214cf7cac8817c0d01cc1aad92069021ba
Ruslan379 <103874267+Ruslan379@users.noreply.github.com>
2025-06-23 14:08:01 +0000

    Deploying to gh-pages from @ Ruslan379/vite-course-fe4-libraries@4b1291b8378a4ea7e668e3a9bc6a9381bb9006d4 🚀

--- assets/index-BeXZP2qt.js
+++ assets/index-BeXZP2qt.js
@@ -1,2 +1,2 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css              *///! Головний скрипт
+import"./reset-CdHmiPVj.js";//! Головний скрипт
 console.log("Підключено скрипт main.js");//! Імпорти додаткових скриптів в проєкт

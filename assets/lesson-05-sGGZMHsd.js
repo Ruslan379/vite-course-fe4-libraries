445dd51bf114dd78edaeba58772592883032904d
Ruslan379 <103874267+Ruslan379@users.noreply.github.com>
2025-06-24 13:10:38 +0000

    Deploying to gh-pages from @ Ruslan379/vite-course-fe4-libraries@a252198dbf552902796d6238d91306ae0c647759 🚀

--- assets/lesson-05-sGGZMHsd.js
+++
@@ -1,2 +0,0 @@
-import"./reset-CdHmiPVj.js";//! Урок-05 - приклад використання бібліотек PNotify, Chart.js, BasicLightBox в проєкті Vite
-console.log("Підключено скрипт lesson-05.js");console.log("%c LESSON-05 - Використання бібліотек PNotify, Chart.js, BasicLightBox ","color: white; background-color: #D33F49");
